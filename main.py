becd65a3fd7290ed3fbe0abc558654d2729916d4
Prasanna-2267 <prasannasaravanan2267@gmail.com>
2026-01-16 22:50:54 +0530

    Modified CORS

--- main.py
+++ main.py
@@ -79,6 +79,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://gluco-ai-dusky.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
